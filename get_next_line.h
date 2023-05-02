91e7289977f4a259ca8b240f59145400a9369373
Rong Jie Soo <rsoo@u81z01s05.42kl.edu.my>
2023-05-02 08:40:35 +0800

    fixed norm errors

--- get_next_line.h
+++ get_next_line.h
@@ -13,21 +13,21 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h> // malloc, free
-#include <unistd.h> // read, close
+# include <stdlib.h> // malloc, free
+# include <unistd.h> // read, close
 // testing
-#include <stdio.h>
-#include <fcntl.h>  // open
+# include <stdio.h>
+# include <fcntl.h>  // open
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 // get_next_line.c
 char	*get_next_line(int fd);
 char	*read_buffer_assign(int fd, char *buffer, int *read_status);
 char	*assign_line(char *buffer, char *line);
 // get_next_line_utils.c
-int     line_len(char *buffer, int position);
+int		line_len(char *buffer, int position);
 
 #endif
